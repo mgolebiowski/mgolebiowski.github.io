289c6460cc5d41bcd76c34133d979f36743a44a2
mgolebiowski <michal.golabek94@gmail.com>
2018-02-27 23:59:15 +0100

    Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,29 +6,29 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './dev/index.js',
+  mode: "production",
   module: {
-  loaders: [
-		{ test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-		{
-			test: /\.scss$/,
-			use: ExtractTextPlugin.extract({
-			  fallback: 'style-loader',
-			  //resolve-url-loader may be chained before sass-loader if necessary
-			  use: [{loader:'css-loader', options: {minimize: true}}, 'sass-loader']
-			})
-		},
-		{ test: /\.css$/, use: ExtractTextPlugin.extract({ fallback: "style-loader",use: "css-loader"})}
-	  ],
-	},
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  optimization: {
+    minimize: true
+  },
   plugins: [
-	  new ExtractTextPlugin("styles.css"),
-    new webpack.optimize.UglifyJsPlugin({minimize: true}),
     new CompressionPlugin({
       test: /(\.js$)|(\.css$)/
-    })
+    }),
+    new ExtractTextPlugin("styles.css")
   ]
 };
