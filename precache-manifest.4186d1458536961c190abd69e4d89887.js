473971442a7dc02897241759622038aae4512ef2
mgolebiowski <michal.golabek94@gmail.com>
2019-09-08 18:54:39 +0200

    Deploy new version

---
+++ precache-manifest.4186d1458536961c190abd69e4d89887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34abea8069499559cbb4db91e86fe33d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa578655de95e377545",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5938aa9369c76cd06c06",
+    "url": "/static/css/main.7cfbcf3b.chunk.css"
+  },
+  {
+    "revision": "ffa578655de95e377545",
+    "url": "/static/js/2.0950a7ff.chunk.js"
+  },
+  {
+    "revision": "5938aa9369c76cd06c06",
+    "url": "/static/js/main.8dd7b774.chunk.js"
+  },
+  {
+    "revision": "e3ddd96f0209e31e8f6f",
+    "url": "/static/js/runtime~main.c53f7d00.js"
+  }
+]);
